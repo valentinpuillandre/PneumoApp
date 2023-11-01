c263bc0387ea6f6d9f388e8f9147bfa171d3938a
valentinpuillandre <97514099+valentinpuillandre@users.noreply.github.com>
2023-11-01 18:36:36 +0100

    change

--- app.py
+++ app.py
@@ -93,6 +93,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-        app.run(debug=True, host="0.0.0.0")
+        app.run(debug=True)
     
 #, port=int(os.environ.get("PORT", 8080))
