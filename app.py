9f6a66be7fb6c6d0e10075af9537b5f9452ded54
valentinpuillandre <97514099+valentinpuillandre@users.noreply.github.com>
2023-11-01 17:58:01 +0100

    Changement

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 import os
 import numpy as np
 import cv2
+from PIL import Image
+from io import BytesIO
 # Keras
 from keras.models import load_model
 from keras.preprocessing import image
@@ -25,32 +27,64 @@ model = load_model(MODEL_PATH)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['POST'])
 def predict_pneumonia():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
+    if 'file' not in request.files:
+        return "Aucun fichier n'a été soumis."
+    file = request.files['file']
+    if not file:
+        return "Le fichier n'a pas été soumis."
 
-            # Charger et prétraiter l'image
-            img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-            img = cv2.resize(img, (150, 150))
-            img = np.array(img) / 255
-            img = img.reshape(-1, 150, 150, 1)
+    if file.filename == '':
+        return "Le nom du fichier est vide."
 
-            # Faire une prédiction avec le modèle
-            prediction = model.predict(img)
-            is_pneumonia = "Normal" if prediction > 0.5 else "Pneumonie"
+    # Read the image from POST data
+    image_bytes = file.read()
+    image = Image.open(BytesIO(image_bytes))
 
-            return render_template('index.html', filename=filename, prediction=is_pneumonia)
+    # Convert the image to grayscale
+    image = image.convert("L")
 
-    return render_template('index.html')
+    # Resize the image
+    image = image.resize((150, 150))
+
+    # Convert the image to a NumPy array
+    img_array = np.array(image) / 255
+    img_array = img_array.reshape(-1, 150, 150, 1)
+
+    # Make a prediction with the model
+    prediction = model.predict(img_array)
+    is_pneumonia = "Normal" if prediction > 0.5 else "Pneumonie"
+    return render_template('index.html', prediction=is_pneumonia)
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def predict_pneumonia():
+#     if request.method == 'POST':
+#         if 'file' not in request.files:
+#             return redirect(request.url)
+#         file = request.files['file']
+#         if file.filename == '':
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+#             file.save(file_path)
+
+#             # Charger et prétraiter l'image
+#             img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+#             img = cv2.resize(img, (150, 150))
+#             img = np.array(img) / 255
+#             img = img.reshape(-1, 150, 150, 1)
+
+#             # Faire une prédiction avec le modèle
+#             prediction = model.predict(img)
+#             is_pneumonia = "Normal" if prediction > 0.5 else "Pneumonie"
+
+#             return render_template('index.html', filename=filename, prediction=is_pneumonia)
+
+#     return render_template('index.html')
 #============================================================================
 
 
